efda00b5b1378ec6b4341511856ed63500a4fb32
Fehmir <fehmir@gmail.com>
2023-09-03 02:49:37 +0200

    Clean code

--- Program.cs
+++ Program.cs
@@ -1,18 +1,13 @@
 using OfficeOpenXml;
-using System;
-using System.ComponentModel;
 
 var builder = WebApplication.CreateBuilder(args);
 // Set the license context for EPPlus
-//LicenseContext licenseContext = LicenseContext.NonCommercial;
 ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
-//LicenseManager.CurrentContext = licenseContext;
 
 builder.Services.AddControllers();
 
 
 // Add services and configuration here
-
 var app = builder.Build();
 
 app.UseStaticFiles();
@@ -20,7 +15,6 @@ app.UseRouting();
 
 
 // Configure your application-specific middleware components here
-
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllerRoute(
